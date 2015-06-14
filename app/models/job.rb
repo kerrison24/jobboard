22a0cdbf95e2143c803fbf1d514fe488a2799f13
kerrison24 <kerrisongarcia@gmail.com>
2015-06-14 21:39:15 +1000

    update job views and add model validations

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,2 +1,11 @@
 class Job < ActiveRecord::Base
+  validates :title, presence: true
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :location, presence: true
+  validates :event_type, presence: true
+  validates :requirements, presence: true,
+                           length: { minimum: 100 }
+  validates :description, presence: true,
+                          length: { minimum: 100 }
 end
