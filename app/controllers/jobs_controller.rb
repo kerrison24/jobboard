baf186586961f9ecfef293f39d719fca01f30d7b
kerrison24 <kerrisongarcia@gmail.com>
2015-06-14 22:24:30 +1000

    complete crud actions for job controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -6,15 +6,39 @@ class JobsController < ApplicationController
   def create
     @job = Job.new(job_params)
 
-    @job.save
-    redirect_to @job
+    if @job.save
+      redirect_to @job
+    else
+      render 'new'
+    end
   end
 
   def new
+    @job = Job.new
+  end
+
+  def edit
+    @job = Job.find(params[:id])
   end
 
   def show
     @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+
+    if @job.update(job_params)
+      redirect_to @job
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @job = Job.find(params[:id]).destroy
+
+    redirect_to jobs_path
   end
 
   private
--- app/models/job.rb
+++ app/models/job.rb
@@ -4,8 +4,6 @@ class Job < ActiveRecord::Base
   validates :end_date, presence: true
   validates :location, presence: true
   validates :event_type, presence: true
-  validates :requirements, presence: true,
-                           length: { minimum: 100 }
-  validates :description, presence: true,
-                          length: { minimum: 100 }
+  validates :requirements, presence: true
+  validates :description, presence: true
 end
